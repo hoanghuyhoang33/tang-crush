accf3f2f05266f72f20f8c6157ffb4afa537342e
hoanghuyhoang33 <85311062+hoanghuyhoang33@users.noreply.github.com>
2021-06-05 03:44:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'tôi biết mà , yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay tôi qua đón cậu đi chơi nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100015460933036',
+    messLink: 'https://www.facebook.com/profile.php?id=100014271273512',
 }
