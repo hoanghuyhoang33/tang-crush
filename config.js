1e87b4cd7fe4db1af13525bb19c0a3ad1dfab0a7
hoanghuyhoang33 <85311062+hoanghuyhoang33@users.noreply.github.com>
2021-06-04 13:21:22 +0700

    yeu cau

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'em à!',
+    introTitle: 'cậu ah!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: '💙chào cậu💙',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙💚',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Still like you <33',
+    btnNo: 'No, you ah :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Sending love to you <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <333333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay anh qua đón em đi chơi nhé.',
+    mess: 'tôi biết mà , yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay tôi qua đón cậu đi chơi nhé.',
+    messDesc: 'toiyeucau',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100014271273512
 }
